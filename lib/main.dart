57501e2f3d2773bf480e7602cc9289ac376076e8
Nate <nate6264@gmail.com>
2023-12-30 14:19:54 -0700

    Update theme

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,23 @@ class TaskApp extends StatefulWidget {
 class _TaskAppState extends State<TaskApp> {
   @override
   Widget build(BuildContext context) {
+    const baseColor = Color.fromRGBO(175, 187, 242, 1);
     return MaterialApp(
-        theme: ThemeData(useMaterial3: true),
+        theme: ThemeData(
+            useMaterial3: true,
+            brightness: Brightness.light,
+            textTheme: const TextTheme(
+                displayLarge:
+                    TextStyle(fontSize: 26, fontWeight: FontWeight.normal),
+                bodyLarge: TextStyle(fontSize: 18, color: Colors.black)),
+            appBarTheme: const AppBarTheme(
+                color: baseColor,
+                titleTextStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+                iconTheme: IconThemeData(color: Colors.white)),
+            colorScheme: ColorScheme.fromSeed(seedColor: baseColor)),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(title: const Text("Task Prioritizer")),
